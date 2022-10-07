9f085013963205efdf0092c70af8c6d0f0e10e1b
Daniel Wood <daniel.a.wood.jr@gmail.com>
2022-10-06 22:42:42 -0700

    update compile.js to allow dynamic versioning

--- compile.js
+++ compile.js
@@ -1,27 +1,39 @@
 const { compile } = require('nexe');
-const { version } = require('./package.json');
+const packageJson = require('./package.json');
+const fs = require('fs');
+const version = packageJson.version;
 
 const args = process.argv.slice(2);
+let [major, minor, patch, extra] = version.split('.');
+[patch, extra = ''] = patch.split('-');
+if (args.includes('major')) { major = Number(major) + 1; minor = 0; patch = 0; extra = ''; }
+if (args.includes('minor')) { minor = Number(minor) + 1; patch = 0; extra = ''; }
+if (args.includes('patch')) { patch = Number(patch) + 1; extra = ''; }
+if (args.includes('beta')) { extra = '-beta' }
+if (args.includes('alpha')) { extra = '-alpha' }
+const newVersion = `${major}.${minor}.${patch}${extra}`;
+
 compile({
   input: './serve.js',
   clean: args.includes('clean'),
   build: args.includes('build'),
   bundle: true,
-  output: `./release/mhmm-v${version}`,
-  name: `mhmm-v${version}`,
-  rc: { PRODUCTVERSION: `${version}` },
+  output: `./release/mhmm-v${newVersion}`,
+  name: `mhmm-v${newVersion}`,
+  rc: { PRODUCTVERSION: `${newVersion}` },
   targets: ['windows'],
   vcBuild: ['release'],
   verbose: true,
-  // targets: ['windows-x64-14.15.3', 'windows-x86-14.15.3'],
   temp: "./.nexe",
   ico: "./public/favicon.ico",
   resources: [
     "./build",
-    // "./public",
   ],
 }).then(() => {
-  console.log('compilation successful')
+  console.log('compilation successful');
+  if (version !== newVersion) {
+    fs.writeFileSync('./package.json', JSON.stringify({...packageJson, version: newVersion}, null, 2));
+  }
 }).catch(err => {
   console.error('compilation err: ', err);
 })

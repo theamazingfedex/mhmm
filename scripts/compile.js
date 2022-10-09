22dba0ee5e3d74f5989ef0f0b907b60a5786a05b
Daniel Wood <daniel.a.wood.jr@gmail.com>
2022-10-08 22:29:34 -0700

    fixed issue where clearing the install directory would cause a white screen.

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,21 +1,6 @@
 const { compile } = require('nexe');
 const packageJson = require('../package.json');
-const fs = require('fs');
-const version = packageJson.version;
-
-const args = process.argv.slice(2);
-let [major, minor, patch, extra] = version.split('.');
-[patch, extra = ''] = patch.split('-');
-if (args.includes('major')) { major = Number(major) + 1; minor = 0; patch = 0; extra = ''; }
-if (args.includes('minor')) { minor = Number(minor) + 1; patch = 0; extra = ''; }
-if (args.includes('patch')) { patch = Number(patch) + 1; extra = ''; }
-if (args.includes('beta')) { extra = '-beta' }
-if (args.includes('alpha')) { extra = '-alpha' }
-const newVersion = `${major}.${minor}.${patch}${extra}`;
-console.log('newVersion: ', newVersion);
-if (version !== newVersion) {
-  fs.writeFileSync('./package.json', JSON.stringify({...packageJson, version: newVersion}, null, 2));
-}
+const newVersion = packageJson.version;
 
 compile({
   input: './scripts/serve.js',
